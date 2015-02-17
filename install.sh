5ed332b5b8ada05a0ffa1aa62070658e23018694
neurosys <blabla>
2015-02-17 14:09:26 +0200

    The install backup is made properly now

--- install.sh
+++ install.sh
@@ -8,11 +8,26 @@ function backup_if_exists()
     fi
 
     file=$1
+    bkp_dir=$2
+    if [ -z $bkp_dir ]
+    then
+        bkp_dir=backup_config_
+    fi
 
     if [ -f $file ]
     then
         echo "$file exists"
-        mv $file ${file}_backup
+        if [ -h $file ]
+        then
+            echo "$file is a symlink removing.."
+            rm "$file"
+        else 
+            if [ ! -d $bkp_dir ]
+            then
+                mkdir $bkp_dir
+            fi
+            mv $file $bkp_dir/$(basename $file)
+        fi
     fi
 }
 
@@ -23,11 +38,13 @@ then
     exit 0
 fi
 
+backup_folder="backup_config_$(date +%Y-%m-%d_%H-%M-%S)"
+
 configs=".Xresources .bash_profile .bashrc .emacs .fetchmailrc .gitconfig .gitignore .minttyrc .msmtprc .muttrc .screenrc .tmux.conf .vimperatorrc"
 for i in $configs
 do
     echo "$i"
-    backup_if_exists ~/$i
+    backup_if_exists ~/$i $backup_folder
     ln -s $PWD/$i ~/$i
     sed -e "s;confs_folder=.*;confs_folder=$PWD/bash_files;" -i "$PWD/.bashrc"
 done
