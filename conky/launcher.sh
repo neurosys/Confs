dc72db6748a36010b0969ab5f3115270caaf2fc1
NeuroSys <bla-bla>
2016-11-21 18:07:32 +0200

    Separate the configs for calendar, based on screen resolution of the primary monitor

--- conky/launcher.sh
+++ conky/launcher.sh
@@ -9,5 +9,9 @@ conky -dq -c $INSERT_PATH/memory_rc
 conky -dq -c $INSERT_PATH/filesystem_rc
 conky -dq -c $INSERT_PATH/network_rc
 conky -dq -c $INSERT_PATH/fortune_rc
-conky -dq -c $INSERT_PATH/calendar_rc
+
+width_of_primary_monitor=$(xrandr | grep primary | grep -o "[0-9]\+x[0-9]\+" | grep -o "^[0-9]\+")
+calendar=calendar_rc_$width_of_primary_monitor
+conky -dq -c $INSERT_PATH/$calendar
+
 #conky -dq -c $INSERT_PATH/mediaplayer_rc
