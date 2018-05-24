9860b5e14f22393f55cb9b36cf073dcbac6b6deb
NeuroSys <you@example.com>
2018-05-24 22:37:17 +0300

    Separate conky configurations in folders for each hostname

--- conky/launcher.sh
+++ conky/launcher.sh
@@ -12,11 +12,12 @@ function get_screen_width()
 
 function launcher()
 {
-    if [ -f $INSERT_PATH/$1_$MACHINE_NAME ]
+    echo "launcher() $1"
+    if [ -f $INSERT_PATH/$MACHINE_NAME/$1 ]
     then
-        conky -dq -c $INSERT_PATH/$1_$MACHINE_NAME
+        conky -dq -c $INSERT_PATH/$MACHINE_NAME/$1
     else
-        conky -dq -c $INSERT_PATH/$1
+        conky -dq -c $INSERT_PATH/default/$1
     fi
 }
 
@@ -42,7 +43,7 @@ launcher system_rc
 launcher memory_rc
 launcher fortune_rc
 
-#launcher calendar_rc
+launcher calendar_rc
 launcher processes_rc
 launcher filesystem_rc
 launcher network_rc
