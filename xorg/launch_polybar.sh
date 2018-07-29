735aad2e71d6bb27c1299c6fd75b0b19125fb314
NeuroSys <you@example.com>
2018-07-30 00:43:03 +0300

    Added a script to launch polybar

---
+++ xorg/launch_polybar.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+nr_of_monitors=$(xrandr | grep connected | wc -l)
+
+if [[ $nr_of_monitors -eq 2 ]]
+then
+    polybar -c ~/.myconfig/xorg/polybar_home.conf mon1 &
+    polybar -c ~/.myconfig/xorg/polybar_home.conf mon2 &
+fi
+
+if [[ $nr_of_monitors -eq 3 ]]
+then
+    polybar -c ~/.myconfig/xorg/polybar_work.conf mon1 &
+    polybar -c ~/.myconfig/xorg/polybar_work.conf mon2 &
+    polybar -c ~/.myconfig/xorg/polybar_work.conf mon3 &
+fi
+
+if [[ $nr_of_monitors -eq 1 ]]
+then
+    polybar -c ~/.myconfig/xorg/polybar_home.conf mon1 &
+fi
