06d982f49656a2ac36e2161d6a0a65e97c0cfbc5
NeuroSys <you@example.com>
2019-08-28 12:39:31 +0300

    Updated the work polybar conf

--- xorg/launch_polybar.sh
+++ xorg/launch_polybar.sh
@@ -10,9 +10,9 @@ fi
 
 if [[ $nr_of_monitors -eq 3 ]]
 then
-    polybar -c ~/.myconfig/xorg/polybar_work.conf mon1 &
-    polybar -c ~/.myconfig/xorg/polybar_work.conf mon2 &
-    #polybar -c ~/.myconfig/xorg/polybar_work.conf mon3 &
+    polybar -c ~/.myconfig/xorg/polybar_work_desktop.conf mon1 &
+    polybar -c ~/.myconfig/xorg/polybar_work_desktop.conf mon2 &
+    polybar -c ~/.myconfig/xorg/polybar_work_desktop.conf mon3 &
 fi
 
 if [[ $nr_of_monitors -eq 1 ]]
