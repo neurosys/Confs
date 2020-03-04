568f437a9d4281c7af4569cd2075d5ae5c54a506
NeuroSys <you@example.com>
2020-03-05 01:07:28 +0200

    Fixed launch polybar to load the temporary config files

--- xorg/launch_polybar.sh
+++ xorg/launch_polybar.sh
@@ -4,18 +4,18 @@ nr_of_monitors=$(xrandr | grep "\<connected\>" | wc -l)
 
 if [[ $nr_of_monitors -eq 2 ]]
 then
-    polybar -c ~/.myconfig/xorg/polybar_home.conf mon1 &
-    polybar -c ~/.myconfig/xorg/polybar_home.conf mon2 &
+    polybar -c ~/.myconfig/xorg/polybar.d/polybar_dormammu.txt mon1 &
+    polybar -c ~/.myconfig/xorg/polybar.d/polybar_dormammu.txt mon2 &
 fi
 
 if [[ $nr_of_monitors -eq 3 ]]
 then
-    polybar -c ~/.myconfig/xorg/polybar_work_desktop.conf mon1 &
-    polybar -c ~/.myconfig/xorg/polybar_work_desktop.conf mon2 &
-    polybar -c ~/.myconfig/xorg/polybar_work_desktop.conf mon3 &
+    polybar -c ~/.myconfig/xorg/polybar.d/polybar_camza-pc.txt mon1 &
+    polybar -c ~/.myconfig/xorg/polybar.d/polybar_camza-pc.txt mon2 &
+    polybar -c ~/.myconfig/xorg/polybar.d/polybar_camza-pc.txt mon3 &
 fi
 
 if [[ $nr_of_monitors -eq 1 ]]
 then
-    polybar -c ~/.myconfig/xorg/polybar_home.conf mon1 &
+    polybar -c ~/.myconfig/xorg/polybar.d/polybar_dementor.txt mon1 &
 fi
